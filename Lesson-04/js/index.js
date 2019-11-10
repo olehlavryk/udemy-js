366720c2b17d7135a71866c3d85702da59b76be3
oleh lavrik <oleh.lavrik@gmail.com>
2019-11-10 08:56:15 +0200

    Learnt constructon if, switch

---
+++ Lesson-04/js/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let num = null;
+
+// if(num < 50) {
+//     console.log('Less');
+// } else if(num > 50) {
+//     console.log('More');
+// } else {
+//     console.log('Equal!');
+// }
+
+// num == 50 ? console.log('Equal!') : console.log('Not equal!'); 
+
+switch(num) {
+    case 45:
+        console.log('Less');
+        break;
+    case 55:
+        console.log('More');
+        break;
+    case 50:
+        console.log('Equal');
+        break;
+    default:
+        console.log('An other value');
+}
+   
