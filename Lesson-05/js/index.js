6171a538c0a6cb73546b29d5e1975d01900578eb
oleh lavrik <oleh.lavrik@gmail.com>
2019-11-16 07:35:17 +0200

    Changed home project, add new functions.

--- Lesson-05/js/index.js
+++ Lesson-05/js/index.js
@@ -2,16 +2,16 @@
 
 let num = 10;
 
-// while(num <= 20 ) {
-//     console.log(num);
-//     num++;
-// }
+while(num <= 20 ) {
+    console.log(num);
+    num++;
+}
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while(num <= 12);
+do {
+    console.log(num);
+    num++;
+}
+while(num <= 12);
 
 for(let i = 0; i <= 10; i++) {
     if(i == 5) {
---
+++ home-project/js/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+let money, time;
+
+function start() {
+    
+    while(isNaN(money) || money == '' || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+        time  = prompt('Введите дату в формате YYYY-MM-DD', '');
+    }
+}
+
+start();
+ 
+let appData = {
+        'money': money,
+        'timeData': time,
+        'expenses': {},
+        'optionalExpenses': null,
+        'income': [],
+        'savings': true
+    };
+    
+function chooseExpenses() {
+    for(let i = 0; i < 2; i++) {
+        let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+            b = prompt('Во сколько обойдется?', '');
+
+        if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a != '' && b != '' && a.length < 50)
+        {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+            // decrement index in order to restart iteration
+            i--; 
+        }
+    }
+}
+
+chooseExpenses();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.money / 30).toFixed();
+    alert('Every day money: ' + (appData.money / 30).toFixed());
+}
+
+detectDayBudget();
+
+
+if(appData.moneyPerDay < 100){
+    console.log("Minimum level of living");
+}else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Middle level of living");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High level of living");
+} else {
+    console.log("Whoops, some error occurred!");
+}
+
+function checkSavings() {
+    if(appData.savings == true) {
+        let save    = +prompt('How much money, would you like to save,', ''),
+            persent = +prompt('What is persent', '');
+
+        appData.monthIncome = save/100/12*persent;
+        
+        alert("Income per manth: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+console.log(appData);
