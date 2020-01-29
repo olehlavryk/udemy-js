292a134ad053a14cb9fadc45fb0f28c23b32eaec
oleh lavrik <oleh.lavrik@gmail.com>
2020-01-29 16:36:28 +0200

    added js counters for Yoga landing

---
+++ tabs/js/index.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    function simpleTabs(tabs, tabsParent, tabsContent) {
+
+    }
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for(let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    function showTabContent(b) {
+        if(tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    hideTabContent(1);
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+
+        if(target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if(target == tab[i]) {
+                    
+                      // hide all tab content
+                    hideTabContent(0);
+
+                    // show current tab content
+                    showTabContent(i);
+
+                    break;
+                }
+            }
+        }
+    });
+
+    
+    
+});
