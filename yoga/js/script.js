5bbcffb3e506b09c5b0c863e867a963c9677540b
oleh lavrik <oleh.lavrik@gmail.com>
2020-02-27 10:49:26 +0200

    Add fix to sending for, just use a Promise

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -139,10 +139,37 @@ window.addEventListener('DOMContentLoaded', function() {
 
         form.appendChild(statusMessage);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        
+        function postData(data) {
+
+            return new Promise(function(resolve, reject){
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+               
+                request.addEventListener('readystatechange', function(){
+                    if(request.readyState < 4) {
+                        resolve();
+                        
+                    } else if(request.readyState === 4 && request.status == 200) {
+                        resolve();
+                        
+                    } else {
+                        reject();
+                        
+                    }
+                });
+
+                request.send(data);
+            });
+
+            
+        }
+
+        function clearInputs() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
 
         let formData = new FormData(form);
         let obj = {};
@@ -152,20 +179,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
         let json = JSON.stringify(obj);
 
-        request.send(json);
-
-        request.addEventListener('readystatechange', function(){
-            if(request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if(request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success
-            } else {
-                statusMessage.innerHTML = message.faillure;
-            }
-        });
-
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
+        postData(json)
+            .then(() => statusMessage.innerHTML = message.loading)
+            .then(() => statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.faillure)
+            .then(() => clearInputs());
     });
 });
